cce763e43ca82e21f81264719717486c9c69cf5d
Theron Voran <tvoran@users.noreply.github.com>
2025-07-25 22:55:48 -0700

    clarify `if VAULT_VERSION` logic a bit

--- test/acceptance/setup_suite.bash
+++ test/acceptance/setup_suite.bash
@@ -2,15 +2,15 @@
 
 setup_suite() {
     local INJECTOR_AGENT_VERSION SERVER_VAULT_VERSION CSI_AGENT_VERSION CHART_VALUES
-    if [ -z ${VAULT_VERSION+x} ]; then
+    if [ -n "${VAULT_VERSION}" ]; then
+        INJECTOR_AGENT_VERSION=${VAULT_VERSION}
+        SERVER_VAULT_VERSION=${VAULT_VERSION}
+        CSI_AGENT_VERSION=${VAULT_VERSION}
+    else
         # If VAULT_VERSION is not set, use the defaults from values.yaml
         INJECTOR_AGENT_VERSION=$(yq -r '.injector.agentImage.tag' values.yaml)
         SERVER_VAULT_VERSION=$(yq -r '.server.image.tag' values.yaml)
         CSI_AGENT_VERSION=$(yq -r '.csi.agent.image.tag' values.yaml)
-    else
-        INJECTOR_AGENT_VERSION=${VAULT_VERSION}
-        SERVER_VAULT_VERSION=${VAULT_VERSION}
-        CSI_AGENT_VERSION=${VAULT_VERSION}
     fi
 
     local VAULT_REPOSITORY
